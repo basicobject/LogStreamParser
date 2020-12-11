9550471e0f82690b6a265386d3d16a9c774ef4b8
Deepak Kumar <deepakkumar@gmail.com>
2020-12-11 17:02:54 +0530

    Process multiple files together

--- src/main/scala/LogParser.scala
+++ src/main/scala/LogParser.scala
@@ -1,33 +1,17 @@
-import java.nio.file.{FileSystem, FileSystems, Paths}
+import java.nio.file.{FileVisitOption, Files, Path, Paths}
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.alpakka.file.scaladsl.FileTailSource
-import akka.stream.scaladsl.{FileIO, Source}
+import akka.stream.alpakka.file.scaladsl.Directory
+import akka.stream.scaladsl.{FileIO, Flow, Framing, Source}
 import akka.util.ByteString
-
-import scala.concurrent.TimeoutException
-import scala.concurrent.duration._
-import io.circe._
 import io.circe.parser._
 
 object LogParser extends App {
   implicit val as: ActorSystem = ActorSystem("log-file-parser")
 
-  val path = "/Users/deepakkumar/Downloads/logs1.json"
-  val outputFile = "output-" + path.split("/").last + ".txt"
-
-  val fs = FileSystems.getDefault
-  val lines: Source[String, NotUsed] = FileTailSource
-    .lines(
-      path = fs.getPath(path),
-      maxLineSize = 8192,
-      pollingInterval = 500.millis
-    )
-    .idleTimeout(5.seconds)
-    .recoverWithRetries(0, {
-      case _: TimeoutException => Source.empty
-    })
+  val logRootDir = "/Users/deepakkumar/Downloads/logs_back"
+  val outputFile = "output.txt"
 
   def parseLine(line: String): ByteString = {
     def parseJson: Option[String] = {
@@ -46,7 +30,7 @@ object LogParser extends App {
               if (text.startsWith("c.t.a.m.MediationService:")) {
                 Some(text.split("id=").last)
               } else None
-            case Left(err) => None
+            case Left(_) => None
           }
       }
     }
@@ -57,14 +41,34 @@ object LogParser extends App {
     }
   }
 
-  lines
+  val logFiles: Source[Path, NotUsed] = Directory.walk(
+    Paths.get(logRootDir),
+    Some(1),
+    Seq(FileVisitOption.FOLLOW_LINKS)
+  )
+
+  val fileFilter: Flow[Path, Path, NotUsed] =
+    Flow[Path].filter(path => Files.isRegularFile(path))
+
+  val delimiter = Framing.delimiter(
+    ByteString("\n"),
+    maximumFrameLength = 8192,
+    allowTruncation = true
+  )
+
+  val startedAt = System.currentTimeMillis()
+
+  logFiles
+    .via(fileFilter)
+    .flatMapConcat(FileIO.fromPath(_))
+    .via(delimiter)
+    .map(_.utf8String)
     .map(parseLine)
-    .idleTimeout(5.seconds)
-    .recoverWithRetries(0, {
-      case _: TimeoutException => Source.empty
-    })
     .runWith(FileIO.toPath(Paths.get(outputFile)))
     .andThen {
-      case _ => as.terminate()
+      case _ =>
+        val endAt = System.currentTimeMillis()
+        println(s"Time taken ${endAt - startedAt} millis")
+        as.terminate()
     }(as.dispatcher)
 }
